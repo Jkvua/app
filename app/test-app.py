4c8df4db6ea4608b3ded06ad9bc0b4afb2554fc8
camila ferreira de almeida <mila.e.ferreira28@gmail.com>
2025-05-26 11:56:54 -0300

    feat:alteracao no arquivo main

--- app/test-app.py
+++ app/test-app.py
@@ -7,5 +7,5 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"mensagem": "Hello World! Camila Aqui!!"}
+    assert response.json() == {"mensagem": "Camila Aqui!!"}
 
